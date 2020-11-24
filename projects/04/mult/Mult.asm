ba3f9f779e1213aa1dd4d0a0d8f80ddf7dd05601
darinapo <38864947+darinapo@users.noreply.github.com>
2020-11-24 14:25:40 +0200

    Update Mult.asm
    Adding solution

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -4,6 +4,38 @@
 // File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+@R2
+M = 0
+@R0
+D = M
+@END //If condition is 0 then at least one of  the operands is 0
+D;JEQ
+@i //Saving the index then
+M = D
+@R1
+D = M
+@END
+D;JEQ
+
+(loop)
+@R1
+D = M
+@R2
+M = M + D  //Assging to R2
+@i
+M = M - 1
+D = M
+@loop
+D; JGT
+@END
+0; JMP
+
+(ZERO)
+@R2
+M = 0
+(END)
+@END
+0;JMP
